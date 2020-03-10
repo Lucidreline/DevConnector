050752346100ee3b5a1e4a0a4187cac8fd212306
Manny <ma52castaneda@gmail.com>
2020-03-09 21:53:14 -0700

    imported the login & register pages. We can now enter info into the forms

--- client/src/Components/Layout/Navbar.js
+++ client/src/Components/Layout/Navbar.js
@@ -8,7 +8,7 @@ import {Link} from 'react-router-dom'
         <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
       </h1>
       <ul>
-        <li><Link href="!#">Developers</Link></li>
+        <li><a href="!#">Developers</a></li>
         <li><Link to="/register">Register</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
